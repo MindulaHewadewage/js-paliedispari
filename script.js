ae44db5e585b2b0798eb09571492b27827718dd1
MindulaHewadewage <mindufernando@gmail.com>
2022-12-15 15:22:07 +0100

    step

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log ('JS OK')
+// Metto gli elementi dal dom
+// creare una function per vedere se un numero è pari o dispari
+// faccio scegliere all'utente se vuole pari o dispari
+// genero un numero casuale da 1 a 5 (funzione)
+// faccio la somma
+// si stabilisce se il numero è pari o dispari (funzione)
+// si dichiara il vincitore
