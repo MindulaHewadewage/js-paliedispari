b86dfb379a7ca84bcb7ab4d86839fb79b6167703
MindulaHewadewage <mindufernando@gmail.com>
2022-12-15 16:07:39 +0100

    funzione per generare numero casuale da 1 a 5

--- script.js
+++ script.js
@@ -5,15 +5,39 @@ const userNumberElement = document.getElementById('user-number');
 const button = document.getElementById('button');
 
 
+// genero un numero casuale da 1 a 5 (funzione)
+function getRandomNumber(min = 1 , max = 5){
+    const randomNumber = Math.floor(Math.random()*max +1);
+    return randomNumber;
+}
+const cpuNumber = getRandomNumber();
+console.log('Cpu Number: '+ cpuNumber );
+
+
 // prendo la value dal dom conl'event listener
 button.addEventListener('click' , function(){
     userNumberValue = userNumberElement.value.trim();
     console.log(userNumberValue);
 })
 
-// creare una function per vedere se un numero è pari o dispari
+
 // faccio scegliere all'utente se vuole pari o dispari
-// genero un numero casuale da 1 a 5 (funzione)
+
 // faccio la somma
-// si stabilisce se il numero è pari o dispari (funzione)
+
 // si dichiara il vincitore
+
+
+
+
+
+
+
+// // si stabilisce se la somma è è pari o dispari (funzione)
+// let result = odd;
+// function isEven(number){
+//     if(number % 2 ===0){
+//         result = even;
+//     }
+//     return result;
+// }
